a023ad3641126b4f3dbb36a0b94bc23fd50718b4
skazzp <skazzp@gmail.com>
2023-01-17 20:58:46 +0200

    photo upload working

--- firebase/config.js
+++ firebase/config.js
@@ -1,7 +1,7 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from 'firebase/app';
-// import { getAnalytics } from 'firebase/analytics';
-// import { getStorage } from 'firebase/storage';
+
+import { getFirestore } from 'firebase/firestore';
+import { getStorage } from 'firebase/storage';
 import { getAuth } from 'firebase/auth';
 
 // TODO: Add SDKs for Firebase products that you want to use
@@ -21,6 +21,6 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-// const analytics = getAnalytics(app);
-// export const storage = getStorage();
+export const storage = getStorage(app);
 export const auth = getAuth(app);
+export const db = getFirestore(app);
--- screens/CreatePostsScreen.js
+++ screens/CreatePostsScreen.js
@@ -1,20 +1,50 @@
-import { View, Image, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, Image, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-native';
 import { Camera } from 'expo-camera';
 import { useEffect, useState } from 'react';
 import { Feather } from '@expo/vector-icons';
 import * as Location from 'expo-location';
+import { db, storage } from '../firebase/config';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
 const CreatePostsScreen = () => {
   const [photo, setPhoto] = useState(null);
   const [camera, setCamera] = useState(null);
   const [permission, requestPermission] = Camera.useCameraPermissions();
   const [location, setLocation] = useState(null);
+  const [title, setTitle] = useState(null);
+  const [address, setAddress] = useState(null);
+  const [photoLink, setPhotoLink] = useState(null);
   const [errorMsg, setErrorMsg] = useState(null);
+  const metadata = {
+    contentType: 'image/jpeg',
+  };
 
   const takePhoto = async () => {
     const snap = await camera.takePictureAsync();
     setPhoto(snap.uri);
     console.log('photo', snap, 'permission', permission, 'location', location);
+    let address = await Location.reverseGeocodeAsync(location.coords);
+    setAddress(address[0].city + ', ' + address[0].country);
+  };
+
+  const createPost = async () => {
+    console.log(Date.now());
+    const photoRef = ref(storage, 'photos/' + location.timestamp.toString() + '.jpg');
+    console.log(photoRef);
+    const response = await fetch(photo);
+    const file = await response.blob();
+    const uploadTask = await uploadBytes(photoRef, photo, metadata);
+    // console.log(uploadTask);
+    await getDownloadURL(uploadTask.ref).then(downloadURL => {
+      console.log('Photo available at', downloadURL);
+      setPhotoLink(downloadURL);
+    });
+    // await uploadBytes(photoRef, file, metadata).then(snapshot => {
+    //   getDownloadURL(snapshot.ref).then(downloadURL => {
+    //     console.log('File available at', downloadURL);
+    //     setPhotoLink(downloadURL);
+    //   });
+    // });
   };
 
   useEffect(() => {
@@ -43,21 +73,51 @@ const CreatePostsScreen = () => {
     // </View>
     <View style={styles.container}>
       <View style={styles.cameraBox}>
-        <Camera style={styles.camera} ref={setCamera}>
-          {photo && (
-            <View style={styles.takePhotoContainer}>
-              <Image source={{ uri: photo }} style={{ height: 200, width: 200 }} />
-            </View>
-          )}
-          <TouchableOpacity
-            onPress={takePhoto}
-            // style={styles.snapContainer}
-          >
-            <View style={styles.snapContainer}>
-              <Feather name="camera" size={24} color="#FFFFFF" />
-            </View>
-          </TouchableOpacity>
-        </Camera>
+        {photo ? (
+          <View style={styles.takePhotoContainer}>
+            <Image source={{ uri: photo }} style={styles.camera} />
+          </View>
+        ) : (
+          <Camera style={styles.camera} ref={setCamera}>
+            <TouchableOpacity
+              onPress={takePhoto}
+              // style={styles.snapContainer}
+            >
+              <View style={styles.snapContainer}>
+                <Feather name="camera" size={24} color="#FFFFFF" />
+              </View>
+            </TouchableOpacity>
+          </Camera>
+        )}
+      </View>
+      <View style={styles.titleBox}>
+        <TextInput
+          style={styles.inputTitle}
+          placeholder="Title"
+          placeholderTextColor="#BDBDBD"
+          keyboardType="visible-password"
+          value={title}
+          onChangeText={value => setTitle(value)}
+        />
+      </View>
+      <View style={styles.locationBox}>
+        <TextInput
+          style={styles.inputLocation}
+          placeholder="Location"
+          placeholderTextColor="#BDBDBD"
+          keyboardType="visible-password"
+          value={address}
+          onChangeText={value => setAddress(value)}
+        />
+      </View>
+      <View>
+        <TouchableOpacity
+          onPress={createPost}
+          style={photo ? styles.sendBtn : styles.sendBtnDisabled}
+          disabled={!photo}
+        >
+          <Text style={photo ? styles.sendBtnText : styles.sendBtnTextDisabled}>Create post</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
@@ -76,6 +136,7 @@ const styles = StyleSheet.create({
   },
   camera: {
     height: '100%',
+    width: '100%',
     justifyContent: 'center',
     alignItems: 'center',
   },
@@ -91,11 +152,64 @@ const styles = StyleSheet.create({
     alignItems: 'center',
   },
   takePhotoContainer: {
-    position: 'absolute',
-    top: 50,
-    left: 10,
-    borderColor: '#fff',
-    borderWidth: 1,
+    height: '100%',
+    borderRadius: 8,
+    // marginBottom: 8,
+  },
+  titleBox: {
+    marginTop: 33,
+    height: 50,
+  },
+  inputTitle: {
+    borderBottomWidth: 1,
+    paddingVertical: 15,
+    fontSize: 16,
+    fontFamily: 'Roboto-Regular',
+    borderBottomColor: '#E8E8E8',
+    marginHorizontal: 16,
+  },
+  locationBox: {
+    marginTop: 17,
+    height: 50,
+  },
+  inputLocation: {
+    borderBottomWidth: 1,
+    paddingVertical: 15,
+    fontSize: 16,
+    fontFamily: 'Roboto-Regular',
+    borderBottomColor: '#E8E8E8',
+    marginHorizontal: 16,
+  },
+  sendBtn: {
+    height: 50,
+    borderRadius: 10,
+    marginTop: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#FF6C00',
+    borderRadius: 100,
+    marginHorizontal: 16,
+  },
+  sendBtnDisabled: {
+    height: 50,
+    borderRadius: 10,
+    marginTop: 32,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#F6F6F6',
+    borderRadius: 100,
+    marginHorizontal: 16,
+  },
+  sendBtnText: {
+    color: '#FFFFFF',
+    fontSize: 20,
+    fontSize: 16,
+    fontFamily: 'Roboto-Regular',
+  },
+  sendBtnTextDisabled: {
+    color: '#BDBDBD',
+    fontSize: 16,
+    fontFamily: 'Roboto-Regular',
   },
 });
 
